ea058b005bc2a794d750f86166b827e48b81a845
Aaqibhafeezkhan <aaqibhafeezkhan@gmail.com>
2026-04-11 11:28:24 -0700

    feat: manually implement single-spa lifecycles for svelte

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
-import App from './App.svelte';
+import App from './CollabApp.svelte';
 
-const app = new App({
-  target: document.body,
-});
+let app;
 
-export default app;
+export async function bootstrap(props) {
+}
+
+export async function mount(props) {
+  app = new App({
+    target: props.domElement || document.getElementById('single-spa-container') || document.body,
+    props: {
+        name: props.name
+    }
+  });
+}
+
+export async function unmount(props) {
+  if (app) {
+    app.$destroy();
+  }
+}
