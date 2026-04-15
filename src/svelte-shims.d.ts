b4e5834dbc9a75dce59028b123c9af680a511921
Aaqibhafeezkhan <aaqibhafeezkhan@gmail.com>
2026-04-15 21:04:39 +0530

    feat: initial svelte scaffolding and project config

--- src/svelte-shims.d.ts
+++ src/svelte-shims.d.ts
@@ -1,4 +1,7 @@
 declare module "*.svelte" {
     import { SvelteComponentTyped } from "svelte";
-    export default class extends SvelteComponentTyped<any, any, any> {}
+    export default class extends SvelteComponentTyped<any, any, any> {
+        constructor(options: any);
+        $destroy(): void;
+    }
 }
